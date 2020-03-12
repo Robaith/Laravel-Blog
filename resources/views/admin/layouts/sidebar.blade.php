3ab200c2d911540e04654b21bdf643bc67327044
Robaith <robaith@gmail.com>
2020-03-12 20:23:05 +0600

    Admin and user controllers created, the game begins

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -27,10 +27,10 @@
         <li class="header">MAIN NAVIGATION</li>
         <li class="active treeview">
           
-            <li class="active"><a href="index.html"><i class="fa fa-circle-o"></i>Posts</a></li>
-            <li class="active"><a href="index.html"><i class="fa fa-circle-o"></i>Categories</a></li>
-            <li class="active"><a href="index.html"><i class="fa fa-circle-o"></i>Tags</a></li>
-            <li class="active"><a href="index.html"><i class="fa fa-circle-o"></i>Users</a></li>
+            <li class="active"><a href="{{ route('post.index') }}"><i class="fa fa-circle-o"></i>Posts</a></li>
+            <li class="active"><a href="{{ route('category.index') }}"><i class="fa fa-circle-o"></i>Categories</a></li>
+            <li class="active"><a href="{{ route('tag.index') }}"><i class="fa fa-circle-o"></i>Tags</a></li>
+            <li class="active"><a href="{{ route('user.index') }}"><i class="fa fa-circle-o"></i>Users</a></li>
           
         </li>
         
